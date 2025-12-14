810586ed61b1620bfb08ce3e6b998fb9999e0683
ToprakGngr <b2210356037@cs.hacettepe.edu.tr>
2025-12-14 19:48:13 +0300

    feat: add opcode function table to header

--- include/chip8.h
+++ include/chip8.h
@@ -18,8 +18,73 @@ public:
 
     void LoadRom(const char* filename);
     void LoadFont();
-    void Initialize();
     void EventCycle(); 
+    
+    typedef void (Chip8::*Chip8Func)();
+
+    // main table, 0-F 
+    Chip8Func chip8Table[16];
+
+    // tables with multiple entries
+    Chip8Func chip8Table0[16];
+    Chip8Func chip8Table8[16];
+    Chip8Func chip8TableE[16];
+    Chip8Func chip8TableF[256];
+
+    void Table0();
+    void Table8();
+    void TableE();
+    void TableF();
+
+    // empty-wrong functions
+    void OP_NULL();
+
+    // 0 Series
+    void OP_0NNN(); // SYS addr (ignored by modern interpreters)
+    void OP_00E0(); // CLS (Clear Display)
+    void OP_00EE(); // RET (Return from Subroutine)
+
+    // 1 - 7 Series (Standard Commands)
+    void OP_1NNN(); // JP addr
+    void OP_2NNN(); // CALL addr
+    void OP_3XNN(); // SE Vx, byte (kk = NN)
+    void OP_4XNN(); // SNE Vx, byte
+    void OP_5XY0(); // SE Vx, Vy
+    void OP_6XNN(); // LD Vx, byte
+    void OP_7XNN(); // ADD Vx, byte
+
+    // 8 Series (Mathematical and Bitwise operations)
+    void OP_8XY0(); // LD Vx, Vy
+    void OP_8XY1(); // OR Vx, Vy
+    void OP_8XY2(); // AND Vx, Vy
+    void OP_8XY3(); // XOR Vx, Vy
+    void OP_8XY4(); // ADD Vx, Vy
+    void OP_8XY5(); // SUB Vx, Vy
+    void OP_8XY6(); // SHR Vx
+    void OP_8XY7(); // SUBN Vx, Vy
+    void OP_8XYE(); // SHL Vx
+
+    // 9 - D Series
+    void OP_9XY0(); // SNE Vx, Vy
+    void OP_ANNN(); // LD I, addr
+    void OP_BNNN(); // JP V0, addr
+    void OP_CXNN(); // RND Vx, byte
+    void OP_DXYN(); // DRW Vx, Vy, nibble
+
+    // E Series (Keypad)
+    void OP_EX9E(); // SKP Vx
+    void OP_EXA1(); // SKNP Vx
+
+    // F Series (Timer, Memory and BCD operations)
+    void OP_FX07(); // LD Vx, DT
+    void OP_FX0A(); // LD Vx, K (Wait for key)
+    void OP_FX15(); // LD DT, Vx
+    void OP_FX18(); // LD ST, Vx
+    void OP_FX1E(); // ADD I, Vx
+    void OP_FX29(); // LD F, Vx (Font)
+    void OP_FX33(); // LD B, Vx (BCD)
+    void OP_FX55(); // LD [I], Vx (Reg Dump)
+    void OP_FX65(); // LD Vx, [I] (Reg Load)
 
 };
 
