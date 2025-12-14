065e277fe017f7638f308f3e50f8d479178b8e05
ToprakGngr <b2210356037@cs.hacettepe.edu.tr>
2025-12-14 17:12:27 +0300

    feat: add initial chip8 stack values to the constructor

--- include/chip8.h
+++ include/chip8.h
@@ -1,14 +1,13 @@
 #include <cstdint>
 
-class Chip8
-{
+class Chip8{
 public:
     uint8_t V[16] {};
     uint8_t memory[4096] {};
     uint16_t I {};
     uint16_t pc {};
     uint16_t stack[16] {};
-    uint8_t stack_pointer {};
+    uint8_t sp {};
     uint8_t delay_timer {};
     uint8_t sound_timer {};
     uint8_t keypad[16] {};
@@ -17,6 +16,8 @@ public:
 
     Chip8(){
         pc = 0x200;
+        I = 0;
+        sp = 0;
     }
 
 };
