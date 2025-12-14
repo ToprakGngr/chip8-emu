ff5ab33a3cbb661d03acdd3316166b7181bc3be7
ToprakGngr <b2210356037@cs.hacettepe.edu.tr>
2025-12-14 17:57:35 +0300

    feat: add fonts and constructor

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,6 +1,42 @@
 #include "chip8.h"
 #include <fstream>
 #include <ios>
+#include <iostream>:
+const unsigned int FONTSET_SIZE = 80;
+
+const uint8_t fontset[FONTSET_SIZE] =
+{
+	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
+void Chip8::LoadFont(){
+    for(unsigned i = 0; i < FONTSET_SIZE; i++){
+        memory[i] = fontset[i];
+    }
+}
+
+Chip8::Chip8(){
+    pc = 0;
+    I = 0;
+    sp = 0;
+    LoadFont();
+}
+
 
 const unsigned int START_POS = 0x200;
 const unsigned int MEM_SIZE = 4096;
@@ -13,22 +49,21 @@ void Chip8::LoadRom(const char* filename) {
 
         const long max_size = MEM_SIZE - START_POS;
 
-        if(size > max_Size){
+        if(size > max_size){
             std::cerr << "Warning: ROM has grown too big, the overflowed data will be discarded.\n";
             size = max_size;
         }
 
-            char* buffer = new char[length];
+            char* buffer = new char[size];
 
             file.seekg(0, std::ios_base::beg);
             file.read(buffer, length);
             file.close();
 
-            for(long i = 0; i < length(); i++){
+            for(long i = 0; i < size; i++){
                 memory[START_POS + i] = static_cast<uint8_t>(buffer[i]);
             }
 
         delete[] buffer;
     }
 }
-
