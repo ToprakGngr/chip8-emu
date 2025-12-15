cf932c6b2a01667b4908b54940b4982768000678
ToprakGngr <b2210356037@cs.hacettepe.edu.tr>
2025-12-15 16:16:22 +0300

    fix: fix typos

--- src/ops_control.cpp
+++ src/ops_control.cpp
@@ -1,24 +1,24 @@
 #include "chip8.h"
-
+#include <cstring>
 
 void Chip8::OP_00E0(){
-    memset(display, 0, sizeof(display);
+    memset(display, 0, sizeof(display));
 }
 
 
-void CHip08::OP_00EE(){
+void Chip8::OP_00EE(){
     pc = stack[sp];
     sp--;
 }
 
 
-void Chip08::OP_1NNN(){
+void Chip8::OP_1NNN(){
     uint16_t address = (opcode & 0x0FFF);
-    PC = address;
+    pc = address;
 }
 
 
-void Chip08::OP_2NNN(){
+void Chip8::OP_2NNN(){
     uint16_t address = (opcode & 0x0FFF);
     stack[sp] = pc;
     sp++;
@@ -26,7 +26,7 @@ void Chip08::OP_2NNN(){
 }
 
 
-void Chip08::OP_3XNN(){
+void Chip8::OP_3XNN(){
     uint8_t Vx = (opcode & 0x0F00) >> 8;
     uint8_t kk = (opcode & 0x00FF);
 
@@ -34,7 +34,7 @@ void Chip08::OP_3XNN(){
 }
 
 
-void Chip08::OP_4XNN(){
+void Chip8::OP_4XNN(){
     uint8_t Vx = (opcode & 0x0F00) >> 8;
     uint8_t kk = (opcode & 0x00FF);
 
@@ -42,7 +42,7 @@ void Chip08::OP_4XNN(){
 }
 
 
-void Chip08::OP_5XY0(){
+void Chip8::OP_5XY0(){
     uint8_t Vx = (opcode & 0x0F00) >> 8;
     uint8_t Vy = (opcode & 0x00F0) >> 4;
 
@@ -50,7 +50,7 @@ void Chip08::OP_5XY0(){
 }
 
 
-void Chip08::OP_9XY0(){
+void Chip8::OP_9XY0(){
     uint8_t Vx = (opcode & 0x0F00) >> 8;
     uint8_t Vy = (opcode & 0x00F0) >> 4;
 
@@ -68,27 +68,9 @@ void Chip8::OP_EX9E(){
     if(keypad[V[Vx]]) pc += 2;
 }
 
-void Chip08::OP_EXA1(){
+void Chip8::OP_EXA1(){
     uint8_t Vx = (opcode & 0x0F00) >> 8; 
 
     if(keypad[V[Vx]] == 0) pc += 2;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
